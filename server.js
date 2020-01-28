e3990af6216b36939198dbc86d17da9a59dc3ae5
jenseneducation-richa-bajpai <54705026+jenseneducation-richa-bajpai@users.noreply.github.com>
2020-01-28 02:32:59 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const database = require("./modules/database");
+const endpoints = require("./modules/endpoints");
+const port = process.env.PORT || 8000;
+
+endpoints(app);
+
+app.listen(port, () => {
+  console.log("Server started on port: ", port);
+  database.initiateDatabase();
+});
